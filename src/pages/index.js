9b42dc52c2859faa33216049394784d27c3ebbc5
Desipeli <mr.desipeli@gmail.com>
2024-02-14 14:03:35 +0200

    graphql could be used later, if multiple dicts

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,20 @@ import '../styles/global.css'
 
 import InputGrid from '../components/InputGrid'
 import Settings from '../components/Settings'
-import { graphql } from 'gatsby'
+import WordList from '../components/WordList'
+// import { graphql } from 'gatsby'
+
+import dictJSON from '../data/sanalista.json'
 
 const IndexPage = ({ data }) => {
-  console.log(process.env.GATSBY_BASE_URL)
-  const wordListURL =
-    process.env.GATSBY_BASE_URL +
-    data.allFile.nodes.map((node) => node.publicURL)
+  // console.log(process.env.GATSBY_BASE_URL)
+  // const wordListURL =
+  //   process.env.GATSBY_BASE_URL +
+  //   data.allFile.nodes.map((node) => node.publicURL)
 
   const [rowCount, setRowCount] = React.useState(5)
   const [colCount, setColCount] = React.useState(5)
+  const dictionary = dictJSON.words
 
   return (
     <main className="max-w-4xl mx-auto">
@@ -27,27 +31,28 @@ const IndexPage = ({ data }) => {
           setRowCount={setRowCount}
         />
         <section className="block lg:flex justify-between my-12">
-          <div className="text-xl px-4 lg:my-0 my-12 text-slate-100">
+          <div className="lg:w-1/2 w-full text-xl px-4 lg:my-0 my-12 text-slate-100">
             <p>Valitse kieli ja pelilaudan koko.</p>
             <p>Kirjaa pelilaudan nopissa näkyvät kirjaimet taulukkoon.</p>
           </div>
           <InputGrid rowCount={rowCount} colCount={colCount} />
         </section>
       </section>
+      <WordList dictionary={dictionary} />
     </main>
   )
 }
 
-export const query = graphql`
-  query MyQuery {
-    allFile(filter: { name: { eq: "sanalista" }, extension: { eq: "json" } }) {
-      nodes {
-        base
-        publicURL
-      }
-    }
-  }
-`
+// export const query = graphql`
+//   query MyQuery {
+//     allFile(filter: { name: { eq: "sanalista" }, extension: { eq: "json" } }) {
+//       nodes {
+//         base
+//         publicURL
+//       }
+//     }
+//   }
+// `
 
 export default IndexPage
 
