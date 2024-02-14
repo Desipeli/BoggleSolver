ea25fb6f2e22ed9bea262a0c1611892ba7dc62d2
Desipeli <mr.desipeli@gmail.com>
2024-02-14 14:10:21 +0200

    fixed default value

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -7,7 +7,7 @@ const Settings = ({ rowCount, colCount, setRowCount, setColCount }) => {
   }
 
   return (
-    <section className="xs:flex justify-between">
+    <section className="flex justify-between">
       <select
         name="language"
         id="language"
@@ -18,11 +18,12 @@ const Settings = ({ rowCount, colCount, setRowCount, setColCount }) => {
       <select
         name="grid-size-select"
         id="grid-size-select"
+        defaultValue={5}
         className="w-5/12 border border-gray-300 rounded-lg bg-slate-100 focus:border-green-500 text-xl  min-h-12 hover:shadow-green-500 shadow-md"
         onChange={(e) => handleSelect(e.target.value)}
       >
-        <option value={4}>4 x 4</option>
         <option value={5}>5 x 5</option>
+        <option value={4}>4 x 4</option>
       </select>
     </section>
   )
